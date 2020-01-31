36853719d822f13b8c1e15507b9de9f4c7bac3e7
Terren Culberson <culbersonterren@yahoo.com>
2020-01-31 03:43:43 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,6 +1,6 @@
 # require libraries/modules here
 require 'nokogiri'
-def create_project_hash{
+def create_project_hash
   html = File.read('fixtures/kickstarter.html')
- kickstarter = Nokogiri::HTML(html)}
+ kickstarter = Nokogiri::HTML(html)
 end
