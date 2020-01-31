f0646964775c9ed4f2aa357a3c6d743cdbe3301e
Terren Culberson <culbersonterren@yahoo.com>
2020-01-31 03:47:19 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,8 +4,8 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
  kickstarter = Nokogiri::HTML(html)
 
-project = {}
-
+  project = {}
+end
 kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
     projects[title] = {
